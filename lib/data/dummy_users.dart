15fdd9a2015b8dbfdff93294796d6453e35c4178
Andre de Castro Rondi <andre_castro_rondi@hotmail.com>
2021-05-10 15:07:24 -0400

    Classe Usuário e Dados Fixos

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404_960_720.png',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Andre',
+    email: 'andre@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+  )
+};
