19c97bc4dc7cf4e46534d6db1219257e134af397
Andre de Castro Rondi <andre_castro_rondi@hotmail.com>
2021-05-10 17:03:26 -0400

    Componente Usuário Item

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -6,27 +6,27 @@ const DUMMY_USERS = {
     name: 'Maria',
     email: 'maria@alunos.com.br',
     avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+        "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png",
   ),
   '2': const User(
     id: '2',
     name: 'Rafael',
     email: 'rafael@alunos.com.br',
     avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png',
+        "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_960_720.png",
   ),
   '3': const User(
     id: '3',
     name: 'Fernanda',
     email: 'fernanda@alunos.com.br',
     avatarUrl:
-        'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404_960_720.png',
+        "https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404_960_720.png",
   ),
   '4': const User(
     id: '4',
     name: 'Andre',
     email: 'andre@alunos.com.br',
     avatarUrl:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
   )
 };
